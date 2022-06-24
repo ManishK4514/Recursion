9b89c58cd4cdb79157d597e65198fcead8e2e3a3
Manish <manish80842@gmail.com>
2022-06-24 11:54:56 +0530

    Added question explanation as a comment

--- subsetSum.java
+++ subsetSum.java
@@ -1,4 +1,7 @@
 // Given an array print all the sum of the subset generated from it, in the increasing order
+// Input: N = 3, arr[] = {5,2,1}
+// Output: 0,1,2,3,5,6,7,8
+// Explanation: We have to find all the subset’s sum and print them.in this case the generated subsets are [ [], [1], [2], [2,1], [5], [5,1], [5,2]. [5,2,1],so the sums we get will be  0,1,2,3,5,6,7,8
 
 package recursion;
 
